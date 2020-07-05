1f67c209796b07b73a3083931186b8106c21a830
Nishant Bhasin <nikieme3@gmail.com>
2020-07-05 19:41:52 -0400

    Initial Commit

---
+++ SampleIDFA/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SampleIDFA
+//
+//  Created by Nishant Bhasin on 2020-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
